d32d9e20d416c7086c723809a3f6b88f8516d162
andrei110295@yandex.ru <andrei110295@yandex.ru>
2023-10-22 22:53:46 +0300

    change cameras

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import { PointerLockControls, Sky } from '@react-three/drei';
+import { PointerLockControls, Sky, CameraControls, PerspectiveCamera } from '@react-three/drei';
 import { Physics, RigidBody } from '@react-three/rapier';
 import Ground from './components/Ground';
 import Car from './components/Car';
-import { CarModel } from './models/CarModel';
+import { useLoader } from '@react-three/fiber'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
 function App() {
+  const fbx = useLoader(FBXLoader, '/model.fbx')
+
   return (
     <>
-      <PointerLockControls />
+      <CameraControls minPolarAngle={0} maxPolarAngle={Math.PI / 1.6} />
       <Sky sunPosition={[100, 20, 100]} />
       <ambientLight intensity={3} />
       <Physics gravity={[0, -20, 0]}>
         <Ground />
-        <Car />
-        <RigidBody>
-          <mesh position={[0, 3, -5]}>
-             <boxGeometry />
-          </mesh>
-        </RigidBody>
+        <primitive object={fbx} position={[0, 0, 0]} />
       </Physics>
+      <PerspectiveCamera makeDefault position={[0, 0, 18.5]} />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Canvas } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber'
+import { CameraControls } from '@react-three/drei'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div id="container">
       <div className="point"></div>
-      <Canvas camera={{ fov: 45, position: [0, 4, 15] }} shadows>
+      <Canvas flat shadows>
+        <CameraControls minPolarAngle={0} maxPolarAngle={Math.PI / 1.6} />
         <App />
       </Canvas>
     </div>
